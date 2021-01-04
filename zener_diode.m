922bf4e0e05f3699508088094d00d7a26cab56aa
VIDHI KUMARI <63573906+vidhi09-tech@users.noreply.github.com>
2021-01-04 23:43:10 +0530

    Update zener_diode.m

--- zener_diode.m
+++ zener_diode.m
@@ -72,46 +72,53 @@ subplot(212)
 plot(t,rhs)
 title('rhs')
 
-v1 = cos(pi*t);
-for i = 1:length(t-1)
-    if v1(i) > 0.5;
-        t1(i) =  0.5;
-    elseif v1(i) <= -0.5;
-        t1(i) = -0.5;
-    else t1(i) = v1(i);
-    end
-end
-for k = 1:length(t)
-    if t(k) > 1
-    a(k)=1;
-    else
-    a(k) = 0;
-    end
-end
 
+v1=cos(pi*t);
+for i=1:length(t-1)
+    if v1(i)>0.5;
+        t1(i)=0.5;
+    elseif v1(i)<=-0.5;
+        t1(i)=-0.5;
+    else t1(i)=v1(i);
+    end
+end
+ for k = 1:length(t)
+     if t(k) > 1
+      a(k)=1;
+     else
+      a(k) = 0;
+     end
+ end
+ 
+ 
 q1=a.*t1;
-v2 = cos(pi*(t-1));
-for i = 1:length(t)
-    if v2(i) > 0.5;
-        t2(i) =  0.5;
-    elseif v2(i) <= -0.5;
-        t2(i) = -0.5;
-    else t2(i) = v2(i);
+v2=cos(pi*(t-1));
+for i=1:length(t)
+    if v2(i)>0.5;
+        t2(i)=0.5;
+    elseif v2(i)<=-0.5;
+        t2(i)=-0.5;
+    else t2(i)=v2(i);
     end
 end
+ 
+if(q1==t2)
+    disp('It is a Time-Invariant Systeem')
+else 
+    disp('It is a Time-variant Systeem')
 
-
-
-
-if(q1 == t2)
-    disp(' SYSTEM IS TIME INVARIANT');
-else
-    disp('SYSTEM IS TIME VARIANT');
 end
-
 figure(3)
-subplot(211)
-plot(t,t1)
-subplot(212)
-plot(t,t2)
 
+subplot(411)
+plot(t,v1);
+title('V1 cos(pi*t)');
+subplot(412)
+plot(t,v2);
+title('V2 cos(pi*(t-1)) delayed by 1 unit');
+subplot(413)
+plot(t,q1);
+title('Q1 passed through system then delayed SIGNAL');
+subplot(414)
+plot(t,t2);
+title('T2 FIRST DELAYED THEN PASSED');
